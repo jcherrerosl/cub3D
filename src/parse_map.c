90bcd5b8673ce1a223f77312cfe57df7e2ceafb8
jcherrerosl <jcherrerosl@github.com>
2025-05-09 13:18:02 +0200

    exact size of the map

--- src/parse_map.c
+++ src/parse_map.c
@@ -6,7 +6,7 @@
 /*   By: juanherr <juanherr@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/07 15:03:11 by juanherr          #+#    #+#             */
-/*   Updated: 2025/05/09 13:08:41 by juanherr         ###   ########.fr       */
+/*   Updated: 2025/05/09 13:17:19 by juanherr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,20 +41,45 @@ void	store_player(t_settings *s, int x, int y, char dir)
 	s->map[y][x] = '0';
 }
 
+static int	count_map_lines(const char *filename)
+{
+	int		fd;
+	char	*line;
+	int		count = 0;
+
+	fd = open(filename, O_RDONLY);
+	if (fd < 0)
+		return (-1);
+	line = get_next_line(fd);
+	while (line)
+	{
+		if (is_map_line(line))
+			count++;
+		free(line);
+		line = get_next_line(fd);
+	}
+	close(fd);
+	return (count);
+}
+
+
 void	parse_file(const char *filename, t_settings *s)
 {
-	int		fd = open(filename, O_RDONLY);
+	int		fd;
 	char	*line;
-	char	**map = malloc(sizeof(char *) * 128); // ajusta tamaño si hace falta
+	char	**map;
 	int		map_i;
 	int		x;
 	int		y;
 	char	c;
 
+	fd = open(filename, O_RDONLY);
+	map = malloc(sizeof(char *) * (count_map_lines(filename) + 1));
 	if (fd < 0 || !map)
 		ft_printerror("Error opening file\n");
 	map_i = 0;
-	while ((line = get_next_line(fd)))
+	line = get_next_line(fd);
+	while (line)
 	{
 		if (ft_strncmp(line, "NO ", 3) == 0)
 			s->no = ft_strtrim(line + 2, " \n");
@@ -71,6 +96,7 @@ void	parse_file(const char *filename, t_settings *s)
 		else if (is_map_line(line))
 			map[map_i++] = ft_strtrim(line, "\n");
 		free(line);
+		line = get_next_line(fd);
 	}
 	map[map_i] = NULL;
 	close(fd);
